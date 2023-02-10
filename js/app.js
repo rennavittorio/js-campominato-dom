e6162bf0c24dce2413f2f036226a442bac46bc0e
rennavittorio <renna.vittorio@gmail.com>
2023-02-10 17:15:07 +0100

    EDIT code refactored using functions

--- js/app.js
+++ js/app.js
@@ -3,7 +3,6 @@ let playBtnElement = document.querySelector('.play-btn');
 let userLevelElement = document.getElementById('levelSelection');
 
 
-
 playBtnElement.addEventListener('click', function(){
     //recupero input utente
     let userLevelInput = userLevelElement.value;
@@ -13,100 +12,23 @@ playBtnElement.addEventListener('click', function(){
         alert('please insert all data')
     
     } else {
-
-        //utilizzo gli input validi
-        let sideLenght = 0;
-        let cellsNum = 0; //calcolo griglia
-
-        //creo celle
-        if (userLevelInput === 'easy'){
-            sideLenght = 7;
-        } else if (userLevelInput === 'medium'){
-            sideLenght = 9;
-        } else if (userLevelInput === 'hard') {
-            sideLenght = 10;
-        }
-        cellsNum = sideLenght * sideLenght;
-
-        //controllo livello scelto
-        let levelOutput = 0;
-        if (userLevelInput === 'easy'){
-            levelOutput = Math.round((cellsNum / 100) * 25); //3
-        } else if (userLevelInput === 'medium'){
-            levelOutput = Math.round((cellsNum / 100) * 50); //5
-        } else if (userLevelInput === 'hard') {
-            levelOutput = Math.round((cellsNum / 100) * 75); //8
-        }
+        let sideLenght = setSideLength(userLevelInput);
+        let cellsNum = sideLenght * sideLenght;
         
+        let bombPercent = setBombPercent(userLevelInput, cellsNum);
 
-        let gridElement = document.querySelector('.game-grid') //recupero elemento griglia dal dom
-        gridElement.innerHTML = ``; //resetta campo ad ogni click
+        generateGameField(userLevelInput, cellsNum);
+
+        let bombList = randomUniqueIndexesList(cellsNum, bombPercent);
+        console.log(bombList);
         
-        //creo la griglia dinamica
-        for (let i = 0; i < cellsNum; i++){
-            let num = i + 1;
-        
-            //creo l'elemento
-            const cell = `
-                            <div class="cell cell-${num}" style="width: calc(100% / ${sideLenght})">
-                                ${num}
-                            </div>
-                        ` 
-        
-            gridElement.innerHTML += cell; //lo inserisco nel DOM
-        }
+        let gameFieldElement = document.querySelector('.game-grid');
+        console.log(gameFieldElement);
 
-
-        //genero lista indici random, DEVO RIORDINARLI
-        let myRndIndexesList = randomUniqueIndexesList(cellsNum, levelOutput);
-        console.log('not sorted', myRndIndexesList)
-        myRndIndexesList.sort((a,b)=>a-b);
-        console.log('sorted', myRndIndexesList);
-
-        let bombList = []
-        
-        //genero l'evento click quando sono sulla casella
-        for (let i = 0; i < cellsNum; i++){
-            let cellElement = document.querySelector(`.cell-${i + 1}`);
-
-            //assegno la classe bomb ad ogni indice rnd trovato se corrisponde all'indice delle celle
-            for (let a = 0; a < myRndIndexesList.length; a++){
-                if (myRndIndexesList[a] === i){
-                    cellElement.classList.add('bomb');
-                    bombList.push(cellElement);
-                }
-
-            }
-
-            //aggiungo l'evento click a tutti gli elementi
-            cellElement.addEventListener('click', function(){
-                console.log(i + 1);
-                cellElement.classList.toggle('active')
-
-            })
-            
-            
-        }
-        
-        console.log('this are my bombs', bombList);
-        
-        //se bomba, perdi
-        for(let i = 0; i < bombList.length; i++){
-            let bombElement = bombList[i];
-
-            bombElement.addEventListener('click', function(){
-                if (bombElement.classList.contains('bomb')){
-                    alert('you lose');
-                    gridElement.innerHTML = ``; //resetta campo ad ogni click
-                }
-
-            })
-
-        }
+        gameFieldElement.addEventListener('click', onClick)
 
     }
-    
-    
+
 })
 
 
@@ -136,4 +58,65 @@ function randomUniqueIndexesList(cellsRange, levelOutputRange) {
     }
     
     return result;
+}
+
+
+function generateGameField (userInput, cellsNum){     
+
+    let sideLenght = setSideLength(userInput);
+
+    let gridElement = document.querySelector('.game-grid') //recupero elemento griglia dal dom
+    gridElement.innerHTML = ``; //resetta campo ad ogni click
+    
+    //creo la griglia dinamica
+    for (let i = 0; i < cellsNum; i++){
+        let num = i + 1;
+    
+        //creo l'elemento
+        const cell = `
+                        <div class="cell cell-${num}" style="width: calc(100% / ${sideLenght})">
+                            ${num}
+                        </div>
+                    ` 
+    
+        gridElement.innerHTML += cell; //lo inserisco nel DOM
+    }
+
+}
+
+//set sideLength
+function setSideLength (userInput){
+    let sideLenght = 0;
+    if (userInput === 'easy'){
+        sideLenght = 7;
+    } else if (userInput === 'medium'){
+        sideLenght = 9;
+    } else if (userInput === 'hard') {
+        sideLenght = 10;
+    }
+    
+    return sideLenght;
+}
+
+//set bomb percent
+function setBombPercent (userInput, cellsNum){
+    let levelOutput = 0;
+    if (userInput === 'easy'){
+        levelOutput = Math.round((cellsNum / 100) * 25); //3
+    } else if (userInput === 'medium'){
+        levelOutput = Math.round((cellsNum / 100) * 50); //5
+    } else if (userInput === 'hard') {
+        levelOutput = Math.round((cellsNum / 100) * 75); //8
+    }
+    
+    return levelOutput;
+}
+
+
+//funzione click casella
+function onClick(event){
+    // console.log(event.target);
+    let cell = event.target;
+    cell.classList.add('active');
+
 }
